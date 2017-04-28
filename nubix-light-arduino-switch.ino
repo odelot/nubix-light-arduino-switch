c40fdb5650bff44e193551afa004bbce360bf1cf
odelot <f.odelot@gmail.com>
2017-04-28 11:22:20 -0300

    progress

--- nubix-light-arduino-switch.ino
+++ nubix-light-arduino-switch.ino
@@ -1,15 +1,51 @@
 #include <SPI.h>
 #include "RF24.h"
 #include <avr/sleep.h>
+#include <Arduino.h>
+
+#define DEBUG_NUBIX(...) Serial.println( __VA_ARGS__ )
+
+#ifndef DEBUG_NUBIX
+#define DEBUG_NUBIX(...)
+#define NODEBUG_NUBIX
+#endif
+
 
 int wakePin = 2;                 // pin used for waking up
-int led=13;
+
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
 RF24 radio(9,10);
 /**********************************************************/
 
-byte addresses[][6] = {"1Node","2Node"};
+byte master[6] = {"N"};
+
+
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
 
 void wakeUpNow() {
   // execute code here after wake-up before returning to the loop() function
@@ -41,12 +77,13 @@ int getChannel () {
 
 int getAddress () {
   int ret = 0;
-  ret += digitalRead (A7);
-  ret += digitalRead (A6) * 2;
-  ret += digitalRead (A3) * 4;
-  ret += digitalRead (A2) * 8;
-  ret += digitalRead (A1) * 16;
-  ret += digitalRead (A0) * 32;
+  ret += digitalRead (A0);
+  ret += digitalRead (A1) * 2;
+  ret += digitalRead (A2) * 4;
+  ret += digitalRead (A3) * 8;
+  ret += digitalRead (A4) * 16;
+  ret += digitalRead (A5) * 32;
+ 
   return ret;
 }
 
@@ -60,6 +97,7 @@ void setup() {
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
   pinMode(A0, INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
   pinMode(A2, INPUT_PULLUP);
   pinMode(A3, INPUT_PULLUP);
   pinMode(A4, INPUT_PULLUP);
@@ -74,22 +112,23 @@ void setup() {
  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
   radio.setPALevel(RF24_PA_LOW);
   
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  //radio.openWritingPipe(addresses[1]);
- 
-  
-  // Start the radio listening for data
-  //radio.startListening();
-}
+ }
 
 void loop() {
   sleepNow();     // sleep function called here
   radio.powerUp(); 
   int channel = getChannel ();  
-  int address = getAddress ();  
-
-  
-
-
+  int address = getAddress ();
+  DEBUG_NUBIX (channel);
+  DEBUG_NUBIX (address);
+  memset (master,0,6);
+  master[0]='N';
+  master[2]=address;
+  radio.setChannel (channel);
+  radio.openWritingPipe(master);
+  long battery = readVcc();
+  DEBUG_NUBIX (battery);
+  radio.write( &battery, sizeof(long) );
+  radio.powerDown();
 } // Loop
 
